10e75bc780efc254766f9a086bcd20eceab14759
Nicolas Criticos <nicolas.criticos98@gmail.com>
2026-04-29 18:02:11 +0200

    fix: restore recurrence field in task inserts (column to be added via SQL)

--- src/components/projects/ProjectDetail.tsx
+++ src/components/projects/ProjectDetail.tsx
@@ -89,6 +89,7 @@ export default function ProjectDetail({ project, tasks }: ProjectDetailProps) {
       scheduled_date: newDate || null,
       completed_at: null,
       dropped_reason: null,
+      recurrence: newRecurrence || null,
     })
     setNewTitle('')
     setNewPriority('normal')
